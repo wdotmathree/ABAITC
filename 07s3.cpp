3cd51bcf3afcf8103b12e6baa02948a4f9a083ae
wdotmathree <wdotmathree@gmail.com>
2021-11-23 18:43:35 -0700

    Add solution for 2007 S3 - Friends

--- 07s3.cpp
+++ 07s3.cpp
@@ -3,7 +3,53 @@
  * S3 - Friends
  * https://dmoj.ca/problem/ccc07s3
  * 
- * Status: Not yet attempted
+ * Status: Completed
  * Language: C++
+ * Points: 40/40
+ * Submission: https://dmoj.ca/submission/4058595
  * 
  */
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int assignments[10000];
+
+int main()
+{
+  int a, b;
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a >> b;
+    assignments[a] = b;
+  }
+  while (true)
+  {
+    cin >> a >> b;
+    if (a == 0)
+      break;
+    int t = assignments[a];
+    int count = 0;
+    set<int> s;
+    while (true)
+    {
+      if (s.find(t) != s.end())
+      {
+        cout << "No" << endl;
+        break;
+      }
+      s.insert(t);
+      if (t == b)
+      {
+        cout << "Yes " << count << endl;
+        break;
+      }
+      t = assignments[t];
+      count++;
+    }
+  }
+  return 0;
+}
