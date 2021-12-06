dcf0eeed8aa481ce45a250886cc6ec3367a69508
wdotmathree <wdotmathree@gmail.com>
2021-12-05 21:31:02 -0700

    Add partial solution for 2016 S3 - Phonomenal Reviews

--- 16s3.cpp
+++ 16s3.cpp
@@ -7,3 +7,16 @@
  * Language: C++
  * 
  */
+
+#include <iostream>
+using namespace std;
+
+int n, m;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> n, m;
+}
