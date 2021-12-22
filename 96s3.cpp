7aa08c93551ac297cd5bc6e5d9c42ba5872f15e2
wdotmathree <wdotmathree@gmail.com>
2021-12-22 12:30:20 -0700

    Add solutions to longest substring and 1996 S3 - Pattern Generator

---
+++ 96s3.cpp
@@ -0,0 +1,65 @@
+/**
+ * CCC 1996
+ * S3 - Pattern Generator
+ * https://dmoj.ca/problem/ccc96s3
+ * 
+ * Status: Completed
+ * Language: C++
+ * Points: 5/5
+ * Submission: https://dmoj.ca/submission/4128946
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> generate(int n, int k)
+{
+  vector<string> ans;
+  if (k == 0)
+  {
+    string s = "";
+    for (int i = 0; i < n; i++)
+      s.push_back('0');
+    ans.push_back(s);
+    return ans;
+  }
+  if (n == k)
+  {
+    string s = "";
+    for (int i = 0; i < n; i++)
+      s.push_back('1');
+    ans.push_back(s);
+    return ans;
+  }
+  auto pre = generate(n - 1, k - 1);
+  for (string s : pre)
+  {
+    s = "1" + s;
+    ans.push_back(s);
+  }
+  pre = generate(n - 1, k);
+  for (string s : pre)
+  {
+    s = "0" + s;
+    ans.push_back(s);
+  }
+  return ans;
+}
+
+int main()
+{
+  int t, n, k;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    cin >> n >> k;
+    cout << "The bit patterns are\n";
+    auto v = generate(n, k);
+    for (auto s : v)
+    {
+      cout << s << "\n";
+    }
+    cout << "\n";
+  }
+}
---
+++ longest_substring.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  unordered_map<char, int> chars;
+  int start = 0;
+  int end = 0;
+  string original;
+  int k;
+  cin >> original >> k;
+  int left = 0;
+  for (int right = 0; right < original.length(); right++)
+  {
+    if (chars.find(original[right]) == chars.end())
+      chars[original[right]] = 1;
+    else
+      chars[original[right]]++;
+    while (chars.size() > k)
+    {
+      if (chars[original[left]] == 1)
+        chars.erase(original[left]);
+      else
+        chars[original[left]]--;
+      left++;
+    }
+    if (right - left + 1 > end - start)
+    {
+      start = left;
+      end = right;
+    }
+  }
+  cout << end - start + 1 << endl;
+}
