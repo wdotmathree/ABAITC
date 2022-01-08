e9857962abaf1227d85936aefb7a8a4efa78b4a3
wdotmathree <wdotmathree@gmail.com>
2022-01-07 21:20:21 -0700

    I thought it would fix it but apparently not

--- 00s4r.cpp
+++ 00s4r.cpp
@@ -5,10 +5,15 @@ short dist, nc, minc = 0x7fff;
 short *clubs;
 
 short recurse(short d) {
+  string a;
   if (d < 0)
     return -1;
   if (d == 0)
     return 0;
+  for (short i = 0; i < nc; i++) {
+    if (d == clubs[i])
+      return 1;
+  }
   short min = 0x7fff;
   for (short i = 0; i < nc; i++) {
     short t = recurse(d - clubs[i]);
